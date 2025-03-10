b013b033bbe167b7fdd748faaada211deee04c17
anjana <anjanavb1@gmail.com>
2025-03-10 23:12:55 +0800

    Adding exception handling with validations

--- EpidemicDiseaseTrackerAPI/EpidemicDiseaseTrackerAPI/Controllers/EpidemicDiseaseCasesController.cs
+++ EpidemicDiseaseTrackerAPI/EpidemicDiseaseTrackerAPI/Controllers/EpidemicDiseaseCasesController.cs
@@ -21,43 +21,96 @@ namespace EpidemicDiseaseTrackerAPI.Controllers
         [HttpGet("yearly")]
         public async Task<IActionResult> GetCasesByYear()
         {
-            var cases = await _repository.GetYearlyCasesAsync();
-            if (!cases.Any()) return NotFound("No data available");
+            try
+            {
+                var cases = await _repository.GetYearlyCasesAsync();
+                if (!cases.Any()) return NotFound("No data available");
 
-            return Ok(cases);
+                return Ok(cases);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500,"An error occurred while processing your request. Please try later");
+            }
         }
 
 
         [HttpGet("year/{year}/weekly")]
         public async Task<IActionResult> GetCasesByYearWeekly(int year)
         {
-            var cases = await _repository.GetWeeklyCasesAsync(year);
-            if (!cases.Any()) return NotFound("No weekly data found for the given year.");
+            try
+            {
+                if (year > 2022 || year < 2012)
+                {
+                    return BadRequest("Year must be between 2022 and 2012.");
+                }
+                var cases = await _repository.GetWeeklyCasesAsync(year);
+                if (!cases.Any()) return NotFound("No weekly data found for the given year.");
 
-            return Ok(cases);
+                return Ok(cases);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while processing your request. Please try later");
+            }
         }
 
         [HttpGet("years")]
         public async Task<IActionResult>GetAvailableYears()
         {
-            var years = await _repository.GetAvailableYearsAsync();
-            if (!years.Any()) return NotFound("No years found.");
+            try
+            {
+                var years = await _repository.GetAvailableYearsAsync();
+                if (!years.Any()) return NotFound("No years found.");
 
-            return Ok(years);
+                return Ok(years);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while processing your request. Please try later");
+            }
         }
         [HttpGet("year/{year}/weekly/{diseaseName}")]
         public async Task<IActionResult>GetWeeklyCasesByDisease(int year,string diseaseName)
         {
-            var cases=await _repository.GetWeeklyCasesByDiseaseNameAsync(year, diseaseName);
-            if (!cases.Any()) return NotFound("No weekly data found for the given year or disease.");
-            return Ok(cases);
+            try
+            {
+
+                if (year > 2022 || year < 2012)
+                {
+                    return BadRequest("Year must be between 2022 and 2012.");
+                }
+                if (string.IsNullOrWhiteSpace(diseaseName))
+                {
+                    return BadRequest("Disease name cannot be empty.");
+                }
+                var cases = await _repository.GetWeeklyCasesByDiseaseNameAsync(year, diseaseName);
+                if (!cases.Any()) return NotFound("No weekly data found for the given year or disease.");
+                return Ok(cases);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while processing your request. Please try later");
+            }
         }
         [HttpGet("year/{year}/diseaseName")]
         public async Task<IActionResult> GetDiseasesForYear(int year)
         {
-            var diseases = await _repository.GetDiseasesForYearAsync(year);
-            if (!diseases.Any()) return NotFound("No diseases found for the given year.");
-            return Ok(diseases);
+            try
+            {
+                if (year > 2022 || year < 2012)
+                {
+                    return BadRequest("Year must be between 2022 and 2012.");
+                }
+
+                var diseases = await _repository.GetDiseasesForYearAsync(year);
+                if (!diseases.Any()) return NotFound("No diseases found for the given year.");
+                return Ok(diseases);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while processing your request. Please try later");
+            }
         }
     }
 }
